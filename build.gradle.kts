2232ebff6d60ab8574a807ee2ac211efc21ee8cc
Trieu Quoc Thinh <126971969+chip1102dz@users.noreply.github.com>
2024-10-27 00:37:14 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.sonarqube") version "5.0.0.4638"
 }
 
 group = "com.gamestudio"
@@ -16,5 +17,11 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+sonar {
+  properties {
+    property("sonar.projectKey", "Onix-Project")
+    property("sonar.projectName", "Onix-Project")
+  }
 }
 
