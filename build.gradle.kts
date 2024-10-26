40b2ce14e8d33e9a5cf19c18e2a51837de98d920
Trieu Quoc Thinh <126971969+chip1102dz@users.noreply.github.com>
2024-10-27 00:20:13 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,9 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
-sonarqube {
-    properties {
-        property ("sonar.projectKey", "rockman-master")
-        property ("sonar.projectName", "rockman-master")
-        property ("sonar.host.url", "http://localhost:9000")
-        property ("sonar.token", "sqp_ad2be4911638e209431361fa814643c749736d12")
-    }
+sonar {
+  properties {
+    property("sonar.projectKey", "Onix-Project")
+    property("sonar.projectName", "Onix-Project")
+  }
 }
