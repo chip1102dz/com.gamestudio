839a7420313c3d20ac05e7f4176bc1696426b56b
Trieu Quoc Thinh <126971969+chip1102dz@users.noreply.github.com>
2024-10-27 22:50:09 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.sonarqube") version "5.0.0.4638"
+    id("org.sonarqube") version "3.5.0.2730"
 }
 
 group = "com.gamestudio"
@@ -20,8 +20,8 @@ tasks.test {
 }
 sonar {
     properties {
-        property("sonar.projectKey", "Onix-Project")
-        property("sonar.projectName", "Onix-Project")
+        property("sonar.projectKey", "TestSonar")
+        property("sonar.projectName", "TestSonar")
         property("sonar.sources", "src/main/java") // Thêm đường dẫn tới mã nguồn
         property("sonar.java.binaries", "build/classes/java/main") // Đường dẫn tới các lớp đã biên dịch
     }
